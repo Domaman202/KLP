8180e06b378576cbc53d05b0963c5c3f9f436e13
DomamaN202 <vip.domaman@mail.ru>
2023-04-01 09:31:30 +0700

    lol

--- compiler/inc/lexer.h
+++ compiler/inc/lexer.h
@@ -3,13 +3,10 @@
 
 #include <token.h>
 
-typedef struct lexer_next_result lexer_next_result_t;
-typedef struct lexer_lex_result lexer_lex_result_t;
-
-struct lexer_next_result {
+typedef struct lexer_next_result {
     char* str;
     struct token* token;
-};
+} lexer_next_result_t;
 
 token_t* token_allocate(char* str, token_t* prev);
 char* token_text(token_t* token);
